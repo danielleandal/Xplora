4e239394874c6253b64bf49d6a1b50e0a4a7d83f
HenriqueLDSa <rique.ldsa@gmail.com>
2024-10-26 19:54:05 -0400

    adding server.js and package.json into a backend/ directory and deleting package-lock.json

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 app.use(cors());
-app.use(bodyParser.tsxon());
+app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
