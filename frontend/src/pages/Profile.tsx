b89993efbe6c4813cab75f7a1772ccf0c9b58850
Krystal Moore-Gayle <krystalmooregayle@gmail.com>
2024-10-28 22:42:07 -0400

    Initial Profile Additions
    Changed HomePage.css to include the sign-out button in .homepage-nav. Added Profile Page to buildinfo.

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,29 +1,71 @@
 import React from 'react';
 import './HowItWorks.css'; // Create this CSS file for styling
+import './Profile.css';
 import logo from '../images/logo.png';
-import { Link } from 'react-router-dom';
+import image1 from '../images/image1.png';
+import { useNavigate, Link } from 'react-router-dom';
 
 const ProfilePage: React.FC = () => {
+    const navigate = useNavigate();
+
+    const handleSignOut = () => {
+        // clear session storage before signing out
+        sessionStorage.clear();
+        navigate('/');
+    };
+    
     return (
         <div className="profile-page">
         {/* Header Section */}
-        <header className="homepage-header">
-        <Link to="/">
-            <img src={logo} alt="Xplora Logo" className="homepage-logo" />
-            </Link>
-            <nav className="homepage-nav">
-            <ul>
-                {/* <li><Link to="/sign-up">Sign Up</Link></li>
-                <li><Link to="/sign-in">Sign In</Link></li> */}
-                <li><Link to="/language">Language</Link></li>
-            </ul>
-            </nav>
-        </header>
+            <header className="homepage-header">
+            <Link to="/">
+                <img src={logo} alt="Xplora Logo" className="homepage-logo" />
+                </Link>
+                <nav className="homepage-nav">
+                <ul>
+                    {/* <li><Link to="/sign-up">Sign Up</Link></li>
+                    <li><Link to="/sign-in">Sign In</Link></li> */}
+                    {/* <li><Link to="/homepage">Sign Out</Link></li> */}
+                    <button className="sign-out-button" onClick={handleSignOut}>Sign Out</button>
+                </ul>
+                </nav>
+            </header>
+            
+            <div id="profile-sidebar" className="profile-sidebar">
+                <a href="#home">Home</a>
+                <a href="#security">Security</a>                
+            </div>
+            
+            <div className="profile-main">
+                <div className="home-container">
+                    <div id="home" className="main-container">
+                        <img src={image1} alt="Profile Picture" className="profile-picture" />
+                        
+                        <div>Name:
+                            <div id="Name"></div>
+                        </div>
+                        <div>Email:
+                            <div id="Email"></div>
+                        </div>
+                        <div>Age:
+                            <div id="Age"></div>
+                        </div>
 
-        <header className="profile-header">
-            <h1>Profile</h1>
-        </header>
-      </div>
+
+                    </div>
+
+                    <div className="main-container">
+                        <div id="security"></div>
+                    </div>
+                </div>
+                
+                
+
+            </div>
+
+        </div>
+
+        
     );
 };
 
