89d79dd48e55767bada85a9969e0e0495921cfee
danielleandal <dannaandal3@gmail.com>
2024-10-29 17:29:42 -0400

    h1 fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,17 +4,16 @@ import HomePage from './pages/HomePage';
 import HowItWorksPage from './pages/HowItWorks';
 import SignUpPage from './pages/SignUp';
 import LoginPage from './pages/Login';
-<<<<<<< HEAD
 import Profile from './pages/Profile'
-=======
 import Dashboard from './pages/Dashboard';
->>>>>>> danielle
 import './App.css';
 
 const App: React.FC = () => {
       // children == dashboard, add profile later
       const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
         const userId = localStorage.getItem('userId');
+
+        // if there is a user stay in the page, if not go back to log in 
         return userId ? <>{children}</> : <Navigate to="/login" />;
     };
   
@@ -24,13 +23,10 @@ const App: React.FC = () => {
         <Route path="/" element={<HomePage/>} />
         <Route path="/how-it-works" element={<HowItWorksPage />} />
         <Route path="/sign-up" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-<<<<<<< HEAD
+        <Route path="/login" element={<LoginPage />} /> 
         <Route path="/profile" element={<Profile />} />
-=======
         {/* Protected Route */}
         <Route path="/dashboard" element={<ProtectedRoute> <Dashboard /></ProtectedRoute>} />
->>>>>>> danielle
       </Routes>
     </Router>
   );
