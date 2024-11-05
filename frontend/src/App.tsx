f7504ea0e9dea12b2d978a3058ba297e5e9bd550
annadelnegro <annaluciadelnegro@gmail.com>
2024-11-04 20:26:25 -0500

    addtrip added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import SignUpPage from './pages/SignUp';
 import LoginPage from './pages/Login';
 import Profile from './pages/Profile'
 import Dashboard from './pages/Dashboard';
+import AddTrip from './pages/AddTrip'
 import './App.css';
 
 const App: React.FC = () => {
@@ -25,6 +26,7 @@ const App: React.FC = () => {
         <Route path="/sign-up" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} /> 
         <Route path="/profile" element={<Profile />} />
+        <Route path="/addtrip" element={<AddTrip />} />
         {/* Protected Route */}
         <Route path="/dashboard" element={<ProtectedRoute> <Dashboard /></ProtectedRoute>} />
       </Routes>
