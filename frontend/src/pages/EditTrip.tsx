bf673402932c435d50619bb2d3a1f8145c473439
annadelnegro <annaluciadelnegro@gmail.com>
2024-11-04 20:41:14 -0500

    commenting EditTrip

--- frontend/src/pages/EditTrip.tsx
+++ frontend/src/pages/EditTrip.tsx
@@ -1,27 +1,27 @@
-// EditTrip.tsx
+// // EditTrip.tsx
 
-import React, { useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import './Dashboard.css';
-import logo from '../images/logo.png';
-import newyork from '../images/new-york.png';
+// import React, { useEffect, useState } from 'react';
+// import { Link, useNavigate } from 'react-router-dom';
+// import './Dashboard.css';
+// import logo from '../images/logo.png';
+// import newyork from '../images/new-york.png';
 
-const EditTrip: React.FC = () => {
-    return (
-        <div className="dashboard">
-            <header className="homepage-header">
-                <img src={logo} alt="Xplora Logo" className="homepage-logo" />
-                <nav className="homepage-nav">
-                    <ul>
+// const EditTrip: React.FC = () => {
+//     return (
+//         <div className="dashboard">
+//             <header className="homepage-header">
+//                 <img src={logo} alt="Xplora Logo" className="homepage-logo" />
+//                 <nav className="homepage-nav">
+//                     <ul>
                        
                        
-                    </ul>
-                </nav>
-            </header>
+//                     </ul>
+//                 </nav>
+//             </header>
 
      
-        </div>
-    );
-};
+//         </div>
+//     );
+// };
 
-export default EditTrip;
+// export default EditTrip;
