9a84d140bbb4a351b93dcc793727b933a7815445
annadelnegro <annaluciadelnegro@gmail.com>
2024-11-25 19:06:40 -0500

    photo submission

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ const storageTrips = multer.diskStorage({
     }
 });
 
-const uploadTripPic = multer({ storageTrips });
+const uploadTripPic = multer({ storage: storageTrips }); 
 
 client.connect();
 
@@ -204,7 +204,7 @@ app.get('/api/users/:id/password', async (req, res) => {
 
 //--------------------------------
 // TRIPS -- POST to add a new trip
-app.post('/api/:userId/trips', uploadTripPic.single('photo'), async (req, res) => {
+app.post('/api/users/:userId/trips', uploadTripPic.single('photo'), async (req, res) => {
     const { userId } = req.params;
     const { name, city, start_date, end_date, notes } = req.body;
 
